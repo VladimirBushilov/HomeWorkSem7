0e00147aadbd00e1b649d48d3b21410b4ca61f7b
VladimirBushilov <ttkshibo@ya.ru>
2023-05-28 18:18:37 +0300

    Решение задач 47, 50

---
+++ Task47/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int ReadText(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] FillMass(int row, int col, double leftRange, double rightRange)
+{
+    double[,] mass = new double[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[i, j] = Math.Round(rand.NextDouble() * 99, 3);
+        }
+    }
+
+    return mass;
+}
+
+void ShowMass(double[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            System.Console.Write(mass[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] matrix = FillMass(ReadText("Введите количество строк: "), ReadText("Введите количество столбцов: "), 0, 99);
+ShowMass(matrix);
---
+++ Task50/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = FillMatrix(ReadInt("Введите количество строк: "), ReadInt("Введите количество столбцов: "), 0, 99);
+PrintMatrix(matrix);
+
+System.Console.WriteLine("Введите позицию элемента массива в формате Х, У: ");
+
+string input = System.Console.ReadLine();
+
+int x = 0;
+int y = 0;
+bool xParsed = false;
+bool yParsed = false;
+for (int i = 0; i < input.Length; i++)
+{
+    if (char.IsDigit(input[i]))
+    {
+        int j = i;
+
+        while (j < input.Length && char.IsDigit(input[j]))
+
+            j++;
+
+        int num = int.Parse(input.Substring(i, j - i));
+
+        if (!xParsed)
+        {
+            x = num;
+            xParsed = true;
+        }
+        else if (!yParsed)
+        {
+            y = num;
+            yParsed = true;
+            break;
+        }
+
+        i = j;
+    }
+}
+
+if (x >= matrix.GetLength(0) || y >= matrix.GetLength(1))
+{
+    Console.WriteLine("Такого элемента не существует.");
+}
+else
+{
+    Console.WriteLine("Значение элемента: " + matrix[x, y]);
+}
